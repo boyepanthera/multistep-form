de5472e6d2ce33fecb09d7d4127f66ca1971659c
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-01-17 20:49:05 +0100

    initialise multistep form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Field, Form, Formik} from 'formik';
+
+
+const formOne = ()=> {
+  if(props.currentForm == 1) {
+    return (
+      <>
+        <Field  name='name' placeholder='Your username'/>
+        <Field  name='email' placeholder='johndoe@gmail.com'/>
+      </>
+    )
+  } else {
+    return null
+  }
+}
+
+const formTwo = ()=> {
+  if(props.currentForm==2) {
+    return (
+      <>
+        <Field  name='address' placeholder='Your address goes here'/>
+        <Field  name='card' placeholder='Card details'/>
+      </>
+    )
+  } else {
+    return null
+  }
+}
+ 
+
+const formThree = props=> {
+  if (props.currentForm == 3) {
+    return (
+      <>
+      <Field name='amount' placeholder='Amount you want to pay'/>
+      <button type='submit'>Submit</button>
+      </>
+    )
+  } else {
+    return null
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Formik
+     initialValues = {{
+       name: '', email: '', address: '', card : '' , amount: '', currentForm: 1
+     }}
+     >
+        {
+          (touched, errors) => (
+            <>
+            <Form>
+              <FormOne currentForm={currentForm} touched={touched} errors={errors} />
+              <FormTwo   currentForm={currentForm} touched={touched}  errors={errors} />
+              <FormThree currentForm={currentForm}  touched={touched}  errors={errors} />
+            </Form>   
+            <backButton/>
+            <nextButton/>
+            </>
+          )
+        }
+
+     </Formik>
     </div>
   );
 }
